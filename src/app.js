f7a78f22f4d2b0fd1ab43af3c3fe93c5283c7f4f
James Strasser <jamesstrasser@Jamess-MBP.fios-router.home>
2020-01-08 20:32:59 -0500

    app.js/src update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -97,5 +97,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port + '.')
+    console.log('Server is up on port ' + port)
 })
